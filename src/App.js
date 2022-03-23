e5c432dba6810aa9526acef44ffd908ae76da1b8
juanjo44 <juanjosebetancourtosorio@gmail.com>
2022-03-22 23:25:24 -0500

    I've worked with first props

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import {Testimonio} from './components/Testimonio'; //Importamos el componente
 
 function App() {
   return (
-    <div className="App">
-      <div className="contenedor-principal">
+    <div className='App'>
+      <div className='contenedor-principa'>
         <h1>Opiniones de los Deportistas</h1>
-        <Testimonio />
+        <Testimonio
+        nombre='Frenkie de Jong'
+        pais='Paises Bajos'
+        imagen=''
+        cargo='Futbolista'
+        equipo='Fc Barcelona'
+        testimonio='Estoy muy contento de estar en el Barça. Desde pequeño quería estar aquí, por lo que he hecho realidad mi sueño. Pero me gustaría haber ganado más títulos en estos dos años. Esperaba más en ese sentido. Pero estoy muy feliz aquí y quiero quedarme muchos años'/>
       </div>
     </div>
   );
--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -2,7 +2,7 @@ import React from "react";
 import '../css/Testimonio.css'
 
 //Componente de funcion, retorn un elemento jsx
-export function Testimonio(){
+export function Testimonio(props){
   return (
     <div className="contenedor-testimonio">
       <img
@@ -10,9 +10,9 @@ export function Testimonio(){
         src={require('../images/de-jong.jpg')} //require porque estamos seleccionando desde el javascript
         alt='Imagen de De Jong'/>
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Frenkie de Jong</p>
-        <p className='cargo-testimonio'>Futbolista Holandes en Fc Barcelona</p>
-        <p className='texto-testimonio'>Estoy muy contento de estar en el Barça. Desde pequeño quería estar aquí, por lo que he hecho realidad mi sueño. Pero me gustaría haber ganado más títulos en estos dos años. Esperaba más en ese sentido. Pero estoy muy feliz aquí y quiero quedarme muchos años</p>
+        <p className='nombre-testimonio'>{props.nombre} de {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en {props.equipo}</p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
       </div>
     </div>
   );
